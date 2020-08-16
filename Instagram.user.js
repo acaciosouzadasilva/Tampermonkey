dac593cdf34dc77d378ba6179f0e6a8310fcfabb
Acacio Souza da Silva <zadfcer666@gmail.com>
2020-08-16 00:02:22 -0300

    Instagram download video

--- Instagram.user.js
+++ Instagram.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
-// @name         Instagram
+// @name         Instagram download video
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
-// @match        https://www.instagram.com/p/*
+// @author       Acacio
+// @match        https://www.instagram.com/*/*/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
+      
     // Your code here...
-      let url = document.querySelector("body > script:nth-child(16)").text.match(/\"has_audio\"\:true\,\"video_url\"\:\"(.+?)\"/)[1];
-      let urlNew = url.replace(/\\u0026/g,"&");
-      console.log(urlNew);
+      const url = document.querySelector("body>script:nth-child(15)").text.match(/\"has_audio\"\:(true|false)\,\"video_url\"\:\"(.+?)\"/)[2];
+      const urlNew = url.replace(/\\u0026/g,"&");
+      document.location.replace(urlNew);
       var win = window.open(urlNew, '_blank');
       win.focus();
 })();
